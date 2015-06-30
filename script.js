32f56a4833089566142f3d146dc594cd337a37de
Rohit Gohri <gohri.rohit@gmail.com>
2015-07-01 00:15:02 +0530

    Added click functions for all buttons, random works now, opacity has some errors, trail stil doesn't work

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready( function() {
-	var n = 16, mode = 'normal';
+	var n = 16;
+	clr = "#D3D3D3"
+	mode = 'normal';
 	$('#container').height($('#container').width() + 'px');
 	function generate(n) {
 		for (var i = n; i > 0; i--) {
 			for (var j = n; j > 0; j--) {
-				$('#container').append("<div class='box'></div>");
+				$('#container').append("<div class='box' data-custom='no' data-per='-0.1'></div>");
 			}
 		}
 		var s = 100/n;
@@ -14,30 +16,62 @@ $(document).ready( function() {
 		$('.box').css("box-sizing","border-box");
 	}
 	generate(n);
-	
-	$('#reset').click( function() {
+	$('#reset').click(function() {
 		var m = prompt("Tell the size of one side of the grid");
 		$('#container').empty();
 		generate(m);
-	})
+		mode = 'normal';
+		$('.box').hover(mouseIn, mouseOut);
+	});
+	$('#random').click(function() {
+		$('.box').css("background-color","LightGray");
+		$('.box').data("custom", "no");
+		mode = 'random';
+		$('.box').hover(mouseIn, mouseOut);
+	});
+	$('#opac').click(function() {
+		$('.box').css("background-color","LightGray");
+		mode = 'opac';
+		$('.box').hover(mouseIn, mouseOut);
+	});
+	$('#trail').click(function() {
+		$('.box').css("background-color","LightGray");
+		mode = 'trail';
+		$('.box').hover(mouseIn, mouseOut);
+	});
 
-	function mouseIn(mode) {
+	function shadeColor(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+	}
+
+	function mouseIn() {
 		if(mode == 'normal' || mode == 'trail') {
 			$(this).css("background-color", "black");
 		}
+		if(mode == 'opac') {
+			var prc = $(this).data("per");
+			var col = shadeColor(clr,prc);
+			$(this).css("background-color", col);
+			if(prc>-1) 
+				prc += -0.1;
+			$(this).data("per", prc);
+		}
 		if(mode == 'random') {
-			var r =Math.floor((Math.random()*256));
-			var g =Math.floor((Math.random()*256));
-			var b =Math.floor((Math.random()*256));
-			var c = 'rgb('+r+','+g+','+b+')'; 
-			$(this).css("background-color", c);
+			if ($(this).data("custom") == "no") {
+				$(this).data("custom", "yes");
+				var r =Math.floor((Math.random()*256));
+				var g =Math.floor((Math.random()*256));
+				var b =Math.floor((Math.random()*256));
+				var c = 'rgb('+r+','+g+','+b+')'; 
+				$(this).css("background-color", c);
+			}
 		}
-
 	}
-	function mouseOut(mode) {
+	function mouseOut() {
 		if(mode == 'trail') {
-			$(this).animate( { backgroundColor: grey }, 500);
+			$(this).animate( { backgroundColor: 'grey' }, 'slow');
 		}
 	}
-	$('.box').on("mouseenter", mouseIn(mode));
+	$('.box').hover(mouseIn, mouseOut);
 });
